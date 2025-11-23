4c28c51a78b73828df84fc43fdb0c5c94358123a
SolitudeKing <yuanyi.first@gmail.com>
2025-11-23 09:12:31 +0800

    jwt认证

--- authentication/jwt_authentication.py
+++ authentication/jwt_authentication.py
@@ -1,10 +1,10 @@
-from rest_framework.authentication import BaseAuthentication
+from django.contrib.auth import get_user_model
 from rest_framework import exceptions
+from rest_framework.request import Request
+from rest_framework.authentication import BaseAuthentication
 from ..utils.crypto.jwt_ import JWTHandler
-from ..utils.cache import DataCache
-from ..utils.util import getUserModel
 
-User = getUserModel()
+# User = get_user_model()
 
 
 class JWTAuthentication(BaseAuthentication):
@@ -12,20 +12,44 @@ class JWTAuthentication(BaseAuthentication):
     JWT 认证
     """
 
-    def authenticate(self, request):
-        token = request.headers.get("Authorization")
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.user_model = get_user_model()
+
+    def authenticate(self, request: Request):
+        token = self.getToken(request)
         if not token:
-            raise exceptions.NotAuthenticated('未提供授权信息')
+            raise exceptions.NotAuthenticated("未提供授权信息")
+        validated_token = self.getValidatedToken(token)
+        return self.getUser(validated_token), token
 
-        login_data = DataCache.getData(token)
+    def getToken(self, request: Request) -> str:
+        """
+        获取令牌
+        """
+        token = request.headers.get("Authorization")
+        if isinstance(token, bytes):
+            token = token.decode()
 
-        if not login_data:
-            raise exceptions.AuthenticationFailed("Token 不存在/已过期，请重新登录！")
-        token_info = JWTHandler().decode(token)
+        return token.strip()
+
+    def getValidatedToken(self, token: str) -> str:
         try:
-            user = User.objects.get(id=token_info["user_id"])
-        except User.DoesNotExist:
-            raise exceptions.AuthenticationFailed("用户不存在！")
+            validated_token = JWTHandler().decode(token, verify_exp=True)
         except Exception as e:
-            raise exceptions.AuthenticationFailed("用户不存在！")
-        return user, token
+            raise exceptions.AuthenticationFailed("令牌已过期！") from e
+
+        return validated_token
+
+    def getUser(self, validated_token: str):
+        """
+        尝试使用已验证的令牌查找并找回用户。
+        """
+
+        try:
+            user = self.user_model.objects.get(id=validated_token["user_id"])
+        except self.user_model.DoesNotExist:
+            raise exceptions.AuthenticationFailed("用户不存在！") from e
+        except Exception as e:
+            raise exceptions.AuthenticationFailed("用户不存在！") from e
+        return user
